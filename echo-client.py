b3882c2637a6d6bc6ca35508883af14bcdd6be26
Guilherme Morbeck <72898220+gui-morbeck@users.noreply.github.com>
2023-07-20 13:17:35 -0300

    Trabalho prático de Redes - UFSJ

---
+++ echo-client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import socket
+
+print("Guilherme Morbeck Rodrigues - TP Redes de Computadores")
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.connect((HOST, PORT))
+	s.sendall(b"Hello,world")
+	data = s.recv(1024)
+
+print("Received", repr(data))
